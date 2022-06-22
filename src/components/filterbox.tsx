962dc9b553daf2c2b4494646d31e06bb80b5d95b
Erik Fecher <efecher@gmail.com>
2022-06-22 16:33:41 -0400

    Update the filterbox
    fix UI of the filterbox and buttons

--- src/components/filterbox.tsx
+++ src/components/filterbox.tsx
@@ -4,17 +4,13 @@ const FilterBox:React.FunctionComponent<FilterBoxProps.FilterBox> = (props) => {
   
     return (
       <form className="grid-x grid-padding-x" onSubmit={props.submitHandler}>
-        <div className="cell medium-12">
-          <div className="input-group">
-            <input id="filterbox-input" type="text" placeholder={props.filterBoxPlaceholder} onChange={props.filterBoxChangeHandler} />
-            <div className="input-group-button">
-              <input className="button" type="submit" value="Submit" />
-              <button onClick={props.resetButtonHandler}  className="button" type="button">Start Over</button>
-            </div>
+        <section className="input-group">
+          <input className="input-group-field" style={{fontSize: "1rem", padding: "2rem 1rem"}} id="filterbox-input" type="text" placeholder={props.filterBoxPlaceholder} onChange={props.filterBoxChangeHandler} />
+          <div className="input-group-button">
+            <input className="button" type="submit" value="Submit" />
+            <button onClick={props.resetButtonHandler}  className="button" type="button">Start Over</button>
           </div>
-          
-        </div>
-        
+        </section>
       </form>
     ); 
 }
