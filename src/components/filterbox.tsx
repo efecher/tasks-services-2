2e2450944764e5c556fdec02b0a4fafc0362998f
Erik Fecher <efecher@gmail.com>
2022-06-22 15:49:27 -0400

    UI updates

--- src/components/filterbox.tsx
+++ src/components/filterbox.tsx
@@ -3,15 +3,16 @@ import * as React from 'react';
 const FilterBox:React.FunctionComponent<FilterBoxProps.FilterBox> = (props) => {
   
     return (
-      <form className="grid-x grid-margin-x" onSubmit={props.submitHandler}>
-        <div className="cell medium-8">
-          <input id="filterbox-input" type="text" placeholder={props.filterBoxPlaceholder} onChange={props.filterBoxChangeHandler} />
-        </div>
-        <div className="cell medium-4">
-          <div className="button-group">
-            <input className="button" type="submit" value="Submit" />
-            <button onClick={props.resetButtonHandler}  className="button" type="button">Start Over</button>
+      <form className="grid-x grid-padding-x" onSubmit={props.submitHandler}>
+        <div className="cell medium-12">
+          <div className="input-group">
+            <input id="filterbox-input" type="text" placeholder={props.filterBoxPlaceholder} onChange={props.filterBoxChangeHandler} />
+            <div className="input-group-button">
+              <input className="button" type="submit" value="Submit" />
+              <button onClick={props.resetButtonHandler}  className="button" type="button">Start Over</button>
+            </div>
           </div>
+          
         </div>
         
       </form>
